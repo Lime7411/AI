ed6442037e30970357114aaf5ff8d6b9f4ac5c96
Lime7411 <radzyslaimonas@gmail.com>
2025-04-21 20:35:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ Programoje:
 
   try {
     const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+      model: "gpt-4-turbo",
       messages: [{ role: "user", content: prompt }],
     });
 
