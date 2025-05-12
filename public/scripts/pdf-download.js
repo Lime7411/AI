ebe6a74f72f2b493148f421f879145b2a69d7d27
Lime7411 <radzyslaimonas@gmail.com>
2025-05-12 21:15:53 +0300

    Update pdf-download.js

--- public/scripts/pdf-download.js
+++ public/scripts/pdf-download.js
@@ -1,4 +1,5 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+import jsPDF from 'jspdf';
+
 // Extract structured data from the HTML response
 function extractProgramData(htmlContent) {
     const parser = new DOMParser();
